dd1577b598526293685566592b5a05a85b78b61a
Javier <javiborreguero96@gmail.com>
2018-10-18 14:45:50 +0200

    Empiezo la creación de la query que permite leer los datos de la tabla personajes

--- ADAT_UD02_A01_Guillermo_Crespo_Javier_Borreguero_Miguel_Martinez/src/HibernateManager/HibernateManager.java
+++ ADAT_UD02_A01_Guillermo_Crespo_Javier_Borreguero_Miguel_Martinez/src/HibernateManager/HibernateManager.java
@@ -16,24 +16,25 @@ public class HibernateManager {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		videojuegos v = new videojuegos();
-		
+
 		v.setID(1);
 		v.setNombre("AnaMari");
 		v.setFecha_Lanzamiento("");
 		v.setDesarrollador("");
 		v.setPlataforma("");
-		
-		
-		personajes  p = new personajes();
-		
+
+		personajes p = new personajes();
+
 		p.setID(1);
 		p.setNombre_Personaje("");
 		p.setjuego();
+        
+		//Creación query necesaria para leer los datos de la tabla videojuegos
 		
 		SessionFactory sf = new Configuration().configure().buildSessionFactory();
 		Session s = sf.openSession();
 		s.beginTransaction();
-		
+
 		Query q = s.createQuery("Select v from videojuegos v");
 		// s.save(e);
 		// s.update(e);
@@ -42,13 +43,26 @@ public class HibernateManager {
 		Iterator empleadositerador = resultado.iterator();
 		while (empleadositerador.hasNext()) {
 			videojuegos vdo = (videojuegos) empleadositerador.next();
-			System.out.println("id: "+vdo.getID()+"\n"  +   "Nombre: "+vdo.getNombre()+"\n"   +"Fecha Lanzamiento: "+vdo.getFecha_Lanzamiento()+"\n"   
-			+ "Desarollador: "+vdo.getDesarrollador()+"\n"  + "Plataforma: "+vdo.getPlataforma()+"\n" );
+			System.out.println("id: " + vdo.getID() + "\n" + "Nombre: " + vdo.getNombre() + "\n" + "Fecha Lanzamiento: "
+					+ vdo.getFecha_Lanzamiento() + "\n" + "Desarollador: " + vdo.getDesarrollador() + "\n"
+					+ "Plataforma: " + vdo.getPlataforma() + "\n");
 		}
-	
 		s.getTransaction().commit();
 		s.close();
 		System.exit(0);
+		
+		//Creación query para leer los datos de la tabla personajes
+		
+		SessionFactory sf2 = new Configuration().configure().buildSessionFactory();
+		Session s2 = sf2.openSession();
+		s2.beginTransaction();
+		Query q2 = s2.createQuery("Select p from personajes p");
+		List resultado2 = q2.list();
+		Iterator personajesIterator = resultado2.iterator();
+		while (personajesIterator.hasNext()) {
+			personajes pnj = (personajes) personajesIterator.next();
+		}
+		
 	}
 
 }
